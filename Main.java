595b9df68e7100a8c4c32d86e13bede28c2c7102
IanMcT <ian.mctavish@tldsb.on.ca>
2022-09-22 01:57:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("\033[3mText goes here\033[0m");
+    System.out.println("and  \033[1mBold Text goes here\033[0m  - and next...");
+    System.out.println("\033[4mUnderlined Text goes here\033[0m");
+  }
+}
